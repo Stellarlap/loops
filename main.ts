6d976722c65a275a3701a50a98259d19e3989d74
Stellarlap <100299314+Stellarlap@users.noreply.github.com>
2022-04-12 11:02:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        sprite.move(-1)
+        basic.pause(500)
+    }
+    for (let index = 0; index < 4; index++) {
+        sprite.change(LedSpriteProperty.Y, 1)
+        sprite.change(LedSpriteProperty.X, 1)
+        basic.pause(500)
+    }
+    for (let index = 0; index < 4; index++) {
+        sprite.change(LedSpriteProperty.X, 1)
+    }
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(4, 0)
